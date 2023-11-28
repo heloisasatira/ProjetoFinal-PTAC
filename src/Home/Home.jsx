e3a7cb1bb100516a06252f099648721e5223e7a0
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-11-28 11:13:46 -0300

    Adicionada a constante listaVideo que, primeiramente deve ser lida no localstarage e a informação adicionada na constante "video".

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,66 +2,41 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-   
+    
+    const listaVideo = localStorage.setItem("lista", JSON.stringify(video))},[video]
+    const video ( id )=>{
+       if ( id == id){
+        return listaVideo
+        }
+    }
+
     return (
         <div>
+
+            <div class="navbar">
+                <p><a href="/Cadastro"></a>CADASTRE AQUI</p>
+            </div>
             <h2>EXPOSIÇÃO DE ARTES</h2>
-            <p class="paragrafo">REGISTRE AQUI OS SEUS VÍDEOS FAVORITOS:</p><Link to="/todo">todo</Link>
+
             <div>
            <img class="container2" src="https://www.nucleodoconhecimento.com.br/wp-content/uploads/2019/06/arte-nouveau.jpg"/>
          </div>
 <br></br>
 <br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
     <h3 class="home">Arte</h3>
-    <p class="card-text">A arte (do latim ars) é o conceito que engloba todas as criações realizadas pelo ser humano para expressar uma visão/abordagem sensível do mundo, seja este real ou fruto da imaginação. Através de recursos plásticos, linguísticos ou sonoros, a arte permite expressar ideias, emoções, percepções e sensações.Arte pode ser definida ainda como o reflexo da cultura e da história. Sendo que ela existem em todas as culturas e nos mais diferentes períodos da história. 
-E tal reflexo existe tendo como base os valores estéticos da beleza e da harmonia.</p>
 <figure>
         <img class="card-img-top" src="https://i.pinimg.com/564x/b0/2a/d5/b02ad52919267e6bcd3efc37666307bb.jpg"/>
          </figure>
 
-<iframe>
+<iframe
   width="853"
   height="480"
-  src={'https://www.youtube.com/embed/' video.url.slice(17)}
+  src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
   frameborder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media"
-</iframe>
+></iframe>
 
-       <h3>AS DIVERSAS FORMAS DE EXPRESSÃO</h3>  
-       <p>Na verdade, a arte transforma a forma de imaginar e de entender o mundo. É uma forma de um indivíduo expressar as suas emoções, história e cultura por meio de valores estéticos, como beleza, harmonia e equilíbrio. Por meio da história da arte, é possível conhecer um pouco mais sobre o ser humano através da evolução das suas diversas expressões, ou conhecer a história e as tradições de um país através de suas manifestações culturais.</p> 
-                <div class="box">
-                    <div class="row"> <img src="https://i.pinimg.com/564x/b6/a9/d6/b6a9d68a4d9bfc92bcf9ada696f5ada1.jpg"/>
-                      <p class="paragradoH">Pieter Bruegel, o Velho, foi o artista mais importante da pintura renascentista flamenga e brabantina, um pintor e gravurista da região de Brabante. </p>
-                    </div>
-                </div>     
-                <div class="box">
-                    <div class="row"> <img src="https://i.pinimg.com/564x/42/ed/23/42ed23f148e848af606ec4ba4fa16345.jpg"/>
-                    <p class="paragradoH"> Oscar-Claude Monet foi um pintor francês e o mais célebre entre os pintores impressionistas.</p></div> 
-                </div>
-                <div class="box">
-                    <div class="row"> <img src="https://i.pinimg.com/564x/cf/45/2f/cf452fa7282c9f2aabc3c586491b99ed.jpg"/>
-                    <p class="paragradoH"> Vincent van Gogh foi um importante pintor holandês, um dos maiores representantes do pós-impressionismo. Van Gogh morreu praticamente no anonimato, depois de uma vida atormentada que o levou ao isolamento e finalmente ao suicídio. </p></div> 
-                </div>
-            </div>  
+</div>  
     );
 }
 <footer>Meu Site - Todos os direitos Reservados - 2022</footer>
