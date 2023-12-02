cbea83727b7d8063e1f462cdc3040eb7eab90313
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-12-02 19:57:45 -0300

    Incrementada a função de salvar e renderizar o vídeo na tela do usuário a partir do localstorage e a propriedade map.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,32 +1,22 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import  Card  from "../Componentes/Card";
+import  Letra  from "../Componentes/Letra";
 
 export default function Home() {
     const listaLocalStorage = JSON.parse(localStorage.getItem("lista")) || [];
+    const listaLocalStorageL = JSON.parse(localStorage.getItem("listaLetra")) || [];
 
     return (
         <div>
             <div class="navbar">
-                <p><a href="/Cadastro"></a>CADASTRE AQUI</p>
+                <p><a href="/todo">CADASTRE AQUI</a></p>
             </div>
 
-            <h2>EXPOSIÇÃO DE ARTES</h2>
-
-   {listaLocalStorage.map((exposicao)=> <div> <Card arte={exposicao}/></div>)}{/* renderização do nome */}
-   {listaLocalStorage.map((letraMusica)=> <div> <Card music={letraMusica}/></div>)} {/* renderização da letra */}
-           
-            <div>
-                <img class="container2" src="https://www.nucleodoconhecimento.com.br/wp-content/uploads/2019/06/arte-nouveau.jpg" />
-            </div>
-
-            <br></br>
-            <br></br>
-
-            <h3 class="home">Arte</h3>
-            <figure>
-                <img class="card-img-top" src="https://i.pinimg.com/564x/b0/2a/d5/b02ad52919267e6bcd3efc37666307bb.jpg" />
-            </figure>
+            <h2>EXPOSIÇÃO DE VIDEOS</h2>
+    
+            {listaLocalStorage.map((exposicao)=> <div> <Card arte={exposicao}/></div>)}{/* renderização do nome */}
+            {listaLocalStorageL.map((letraMusica)=> <div> <Letra descricao={letraMusica}/></div>)} {/* renderização da letra */}
 
             {/* renderização do vídeo na tela do usuário */}
             {/* <iframe
@@ -36,8 +26,6 @@ export default function Home() {
   frameborder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media"
 ></iframe> */}
-
-            <footer>Meu Site - Todos os direitos Reservados - 2022</footer>
 
         </div>
     );
