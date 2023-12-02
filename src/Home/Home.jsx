bdd51b30b350f57b1712e382553a960b48d49f15
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-12-02 19:09:17 -0300

    Erro reportado ao colocar o site em execução. Motivo: na função listaVideo, na página Home, há um return inesperado que não satisfaz a condição tida como parâmetro da função.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,42 +1,49 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import  Card  from "../Componentes/Card";
 
 export default function Home() {
-    
-    const listaVideo = localStorage.setItem("lista", JSON.stringify(video))},[video]
-    const video ( id )=>{
-       if ( id == id){
-        return listaVideo
-        }
-    }
+    const listaLocalStorage = JSON.parse(localStorage.getItem("lista")) || [];
+    /*     const video ( id ) => {
+           if ( id == id){
+            return listaVideo;
+            }
+        } */
 
     return (
         <div>
-
             <div class="navbar">
                 <p><a href="/Cadastro"></a>CADASTRE AQUI</p>
             </div>
+
             <h2>EXPOSIÇÃO DE ARTES</h2>
 
+   {listaLocalStorage.map((exposicao)=> <div> <Card arte={exposicao}/></div>)}{/* renderização do nome */}
+   {listaLocalStorage.map((letraMusica)=> <div> <Card music={letraMusica}/></div>)} {/* renderização da letra */}
+           
             <div>
-           <img class="container2" src="https://www.nucleodoconhecimento.com.br/wp-content/uploads/2019/06/arte-nouveau.jpg"/>
-         </div>
-<br></br>
-<br></br>
-    <h3 class="home">Arte</h3>
-<figure>
-        <img class="card-img-top" src="https://i.pinimg.com/564x/b0/2a/d5/b02ad52919267e6bcd3efc37666307bb.jpg"/>
-         </figure>
+                <img class="container2" src="https://www.nucleodoconhecimento.com.br/wp-content/uploads/2019/06/arte-nouveau.jpg" />
+            </div>
 
-<iframe
+            <br></br>
+            <br></br>
+
+            <h3 class="home">Arte</h3>
+            <figure>
+                <img class="card-img-top" src="https://i.pinimg.com/564x/b0/2a/d5/b02ad52919267e6bcd3efc37666307bb.jpg" />
+            </figure>
+
+            {/* renderização do vídeo na tela do usuário */}
+            {/* <iframe
   width="853"
   height="480"
   src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
   frameborder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media"
-></iframe>
+></iframe> */}
 
-</div>  
+            <footer>Meu Site - Todos os direitos Reservados - 2022</footer>
+
+        </div>
     );
 }
-<footer>Meu Site - Todos os direitos Reservados - 2022</footer>
