57f143d3c22a63862581af507b4bfa04aeef19e8
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-12-08 18:01:57 -0300

    Adicionado iframe para renderização dos vídeos.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import  Card  from "../Componentes/Card";
 import  Letra  from "../Componentes/Letra";
+import  Autor  from "../Componentes/Autor";
 
-export default function Home() {
+export default function Home({ video }) {
+
+    const salvar = JSON.parse(localStorage.getItem("videos")) || [];
+
+/*     useEffect()  => {
+      const 
+} */
+
     const listaLocalStorage = JSON.parse(localStorage.getItem("lista")) || [];
     const listaLocalStorageL = JSON.parse(localStorage.getItem("listaLetra")) || [];
+    const listaLocalStorageA = JSON.parse(localStorage.getItem("listaAutor")) || [];
 
     return (
         <div>
@@ -14,19 +23,19 @@ export default function Home() {
             </div>
 
             <h2>EXPOSIÇÃO DE VIDEOS</h2>
-    
-            {listaLocalStorage.map((exposicao)=> <div> <Card arte={exposicao}/></div>)}{/* renderização do nome */}
+            {listaLocalStorage.map((exposicao)=> <div> <Card arte={exposicao}/></div>)}{/* renderização do nome do vídeo */}
             {listaLocalStorageL.map((letraMusica)=> <div> <Letra descricao={letraMusica}/></div>)} {/* renderização da letra */}
+            {listaLocalStorageA.map((autorProprietario)=> <div> <Autor proprietario={autorProprietario}/></div>)} {/* renderização do nome do autor */}
 
-            {/* renderização do vídeo na tela do usuário */}
-            {/* <iframe
-  width="853"
-  height="480"
-  src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
-  frameborder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media"
-></iframe> */}
-
+<iframe
+    width="853"
+    height="480"
+    src={"https://www.youtube.com/embed/" + salvar.url.slice(17)}
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; piccture-in-picture"
+    allowFullScreen
+>
+</iframe>
         </div>
     );
 }
