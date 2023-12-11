db96e564f49cb64ce1b5ffbf3aec493096d865dc
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-12-11 09:46:54 -0300

    Adicionada a propriedade UseEffect, com a função de "dizer" ao react que algo deverá ser feito após a renderização.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,13 +4,12 @@ import  Card  from "../Componentes/Card";
 import  Letra  from "../Componentes/Letra";
 import  Autor  from "../Componentes/Autor";
 
-export default function Home({ video }) {
+export default function Home() {
 
-    const salvar = JSON.parse(localStorage.getItem("videos")) || [];
-
-/*     useEffect()  => {
-      const 
-} */
+    useEffect(() => {
+        const sVideos = JSON.parse(localStorage.getItem('videos')) || [];
+        setVideos(sVideos);
+      }, []);
 
     const listaLocalStorage = JSON.parse(localStorage.getItem("lista")) || [];
     const listaLocalStorageL = JSON.parse(localStorage.getItem("listaLetra")) || [];
@@ -23,19 +22,32 @@ export default function Home({ video }) {
             </div>
 
             <h2>EXPOSIÇÃO DE VIDEOS</h2>
+           
             {listaLocalStorage.map((exposicao)=> <div> <Card arte={exposicao}/></div>)}{/* renderização do nome do vídeo */}
             {listaLocalStorageL.map((letraMusica)=> <div> <Letra descricao={letraMusica}/></div>)} {/* renderização da letra */}
             {listaLocalStorageA.map((autorProprietario)=> <div> <Autor proprietario={autorProprietario}/></div>)} {/* renderização do nome do autor */}
+            
+            {videos.length > 0 ? (
+          videos.map((video, index) => (
+            <div key={index} className="video">
+              <h3>{video.nome}</h3>
 
-<iframe
-    width="853"
-    height="480"
-    src={"https://www.youtube.com/embed/" + salvar.url.slice(17)}
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; piccture-in-picture"
-    allowFullScreen
->
-</iframe>
-        </div>
+          <iframe
+                width="100%"
+                height="200"
+                src={"https://www.youtube.com/embed/" + video.url.slice(17)}
+                title={video.nome}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              <h5>{video.url}</h5>
+            </div>
+          ))
+        ) : (
+
+        )}
+</div>
+
     );
 }
